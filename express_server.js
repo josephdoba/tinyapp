bbce5f6fc7588919ea64956fbb8f35b6037483b5
Joseph Doba <josephdoba@gmail.com>
2022-05-01 16:39:30 -0700

    Added ejs template engine

--- express_server.js
+++ express_server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//set template engine:
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  // res.send("Hello!");
+  res.render('pages/index');
 });
 
 app.get("/urls.json", (req,res) => {
@@ -17,8 +21,19 @@ app.get("/urls.json", (req,res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.render("/hello");
 });
 
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT}!`);
 });
+
+// set & fetch demo:
+// app.get("/set", (req,res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+
+// app.get("/fetch", (req,res) => {
+//   res.send(`a = ${a}`);
+// });
