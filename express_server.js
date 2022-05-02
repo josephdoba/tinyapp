8aabe0856df55ded8d4f4f13c071f494199a6a7d
Joseph Doba <josephdoba@gmail.com>
2022-05-01 17:23:57 -0700

    Added ejs template engine folder hierachy, and some test files for connectivity

--- express_server.js
+++ express_server.js
@@ -10,6 +10,31 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//index
+app.get('/', function(req, res) {
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birthYear: 2012},
+    { name: 'Tux', organization: "Linux", birthYear: 1996},
+    { name: 'Moby Dock', organization: "Docker", birthYear: 2013}
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+});
+
+// about page
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+/*
 app.get("/", (req, res) => {
   // res.send("Hello!");
   res.render('pages/index');
@@ -37,3 +62,6 @@ app.listen(PORT, () => {
 // app.get("/fetch", (req,res) => {
 //   res.send(`a = ${a}`);
 // });
+*/
+
+
