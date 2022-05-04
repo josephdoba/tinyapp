b14b16f1c41379f8b90cd281a2af3c34fcee6133
Joseph Doba <josephdoba@gmail.com>
2022-05-04 13:53:00 -0700

    Added Cookies functionality

--- express_server.js
+++ express_server.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = 8080;
 app.set('view engine', 'ejs');
 
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -51,21 +52,28 @@ app.get('/', (req,res) => {
 // My URL's page:
 app.get("/urls", (req, res) => {
   const templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["username"]
   };
+  console.log(req.cookies);
+  console.log("Inside the /url route handler");
   res.render('urls_index', templateVars);
 });
 
 // Create new URL page:
 app.get('/urls/new', (req,res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render('urls_new', templateVars);
 });
 
 // Individual url page:
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -74,6 +82,26 @@ app.get('/urls/:shortURL', (req, res) => {
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]; // req.params.shortURL is what we need to reference data from the forms
   res.redirect(longURL);
+});
+
+// login process:
+app.post('/login', (req,res) => {
+  let username = "";
+  console.log("The login route is hit");
+  if (username !== req.body.username) {
+    username = req.body.username;
+    res.cookie('username', username);
+  } else {
+    res.clearCookie(username, username);
+    username = req.body.username;
+  }
+  // res.send(`This is the login page, logged in under ${username}`);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req,res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 // ### API Routes ###:
